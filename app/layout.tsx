cbc28c2c37d5df95cc256057e91bd95f66d48877
mendesguilherme <38786114+mendesguilherme@users.noreply.github.com>
2025-06-22 19:32:19 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
-import { MobileMenuProvider } from "@/Context/MobileMenuContext" // ajuste o caminho se necessário
+import { MobileMenuProvider } from "../../Context/MobileMenuContext" // ajuste o caminho se necessário
 
 const inter = Inter({ subsets: ["latin"] })
 
