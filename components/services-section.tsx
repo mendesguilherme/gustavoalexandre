d8b26fdda4e26e9dd07c632c99b5ae826545182a
mendesguilherme <38786114+mendesguilherme@users.noreply.github.com>
2025-06-22 19:09:52 -0300

    Update services-section.tsx

--- components/services-section.tsx
+++ components/services-section.tsx
@@ -1,5 +1,5 @@
 import { Car, Shield, CreditCard, RefreshCw } from "lucide-react"
-import { Card, CardContent } from "@/components/ui/card"
+import { Card, CardContent } from "../components/ui/card"
 
 export function ServicesSection() {
   const services = [
