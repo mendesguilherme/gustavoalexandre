c0da978d2fe0274a2e872a4044fc1758fe21cc42
mendesguilherme <38786114+mendesguilherme@users.noreply.github.com>
2025-06-23 11:11:54 -0300

    Update page.tsx

--- app/veiculos/[id]/page.tsx
+++ app/veiculos/[id]/page.tsx
@@ -14,6 +14,8 @@ import { Badge } from "@/components/ui/badge"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Textarea } from "@/components/ui/textarea"
+import { vehicles } from "@/data/vehicles"
+
 import {
   Calendar,
   Fuel,
