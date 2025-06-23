d96cd082089d0051a249d95fcdd02d1b0ddd124e
mendesguilherme <38786114+mendesguilherme@users.noreply.github.com>
2025-06-22 22:31:08 -0300

    Update MobileMenu.tsx

--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -1,7 +1,7 @@
 "use client"
 import Link from "next/link"
 import { Button } from "../components/ui/button"
-import { useMobileMenu } from "../Context/MobileMenuContext"
+import { useMobileMenu } from "../context/MobileMenuContext"
 
 export function MobileMenu() {
   const { isOpen } = useMobileMenu()
