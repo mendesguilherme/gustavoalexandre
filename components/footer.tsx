8e0a82d36589dfe81ded64a55d4f865371ee42b4
guilhermemendes <geracaoguilherme@gmail.com>
2025-07-03 15:53:09 -0300

    logo rodape

--- components/footer.tsx
+++ components/footer.tsx
@@ -25,7 +25,7 @@ export function Footer({ onOpenSimulacaoModal }: FooterProps) {
   }
 
   return (
-    <footer className="text-white py-2 bg-black">
+    <footer className="text-white py-0 bg-black">
       <div className="container mx-auto px-4">
         <div className="grid md:grid-cols-4 gap-8 items-start">
           {/* Logo e Descrição */}
